b460e2c73f2aa3228e895e32e78eb375bacd0d95
Armaan-Raisinghani <raisinghani.armaan1@gmail.com>
2026-08-09 00:24:19 +0530

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+   public:
+    unordered_map<int, vector<int>> hmap;
+    unordered_set<int> temp_set;
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        for (int i = 0; i < numCourses; i++) hmap[i] = {};
+        for (auto pre : prerequisites) hmap[pre[0]].push_back(pre[1]);
+        for (int c = 0; c < numCourses; c++) {
+            if (!dfs(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool dfs(int c) {
+        if (temp_set.contains(c)) return false;
+        temp_set.insert(c);
+        for (int i : hmap[c]) {
+            if (!dfs(i)) return false;
+        }
+        temp_set.erase(c);
+        hmap[c] = {};
+        return true;
+    }
+};
